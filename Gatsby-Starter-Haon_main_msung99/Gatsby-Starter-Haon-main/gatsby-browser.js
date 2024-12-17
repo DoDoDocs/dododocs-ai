ef3f8d15d31fea1702778a3c4eb52a635450a5ec
Jungmin Lee <starjungmin@gmail.com>
2024-12-17 17:04:08 +0900

    Enhance logging and README processing in `ktb_document_processor.py`, `ktb_func.py`, and `ktb_server.py`: - Added print statements for better debugging and tracking of S3 upload and repository extraction processes. - Updated `parse_repo_url` function to use an underscore instead of a slash for repository and branch separation. - Cleaned up commented code in `ktb_prompts.py` to improve code clarity.

---
+++ Gatsby-Starter-Haon_main_msung99/Gatsby-Starter-Haon-main/gatsby-browser.js
@@ -0,0 +1,10 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
+ */
+
+// You can delete this file if you're not using it
+import "fontsource-noto-sans-kr";
+import "fontsource-noto-sans-kr/700.css";
+import 'katex/dist/katex.min.css';
