bf215c2186d7b9b1740fc8007bccbe4c593ff436
Jungmin Lee <starjungmin@gmail.com>
2024-12-18 18:52:21 +0900

    parse수정

--- lambda_generate/ktb_func.py
+++ lambda_generate/ktb_func.py
@@ -57,7 +57,7 @@ def parse_repo_url(repo_url):
     # Example implementation, adjust based on actual URL structure
     parts = repo_url.rstrip('/').split('/')
     user_name = parts[-3]
-    repo_with_branch = parts[-1]+'_'+parts[-2]
+    repo_with_branch = parts[-2]+'_'+parts[-1]
     return user_name, repo_with_branch
 
 
