6234f7201c1210da3db13591ae9fe4e3fbb5f471
Jungmin Lee <starjungmin@gmail.com>
2024-12-23 14:11:12 +0900

    크리스마스 이벤트

--- lambda_chat.py
+++ lambda_chat.py
@@ -25,6 +25,20 @@ CORS(app, resources={r"/chat": {"origins": origins}},
      supports_credentials=True)
 
 
+def is_christmas():
+    result = ""
+    print('\n'.join
+          ([''.join
+            ([('Merry Christmas '[(x-y) % 8]
+               if ((x*0.05)**2+(y*0.1)**2-1)
+               ** 3 - (x*0.05)**2*(y*0.1)
+               ** 3 <= 0 else ' ')
+                for x in range(-30, 30)])
+              for y in range(15, -15, -1)]))
+    result += "Merry Christmas"
+    return result
+
+
 @app.route('/chat', methods=['POST', 'GET'])
 def chat():
     """채팅 엔드포인트"""
@@ -38,6 +52,10 @@ def chat():
         query = data.get('query')
         chat_history = data.get('chat_history')
         stream = data.get('stream', True)
+
+        # 크리스마스 이스터에그
+        if query == "!christmas":
+            return jsonify({'answer': is_christmas()}), 200
 
         if chat_history:
             chat_history_list = []
