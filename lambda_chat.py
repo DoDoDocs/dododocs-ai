e5956baebb4c4841e7dd44a120ec97a8eeba1b4c
Jungmin Lee <starjungmin@gmail.com>
2024-12-24 06:01:42 +0900

    time no sleep

--- lambda_chat.py
+++ lambda_chat.py
@@ -110,12 +110,12 @@ def chat():
                     chunk_buffer = ""
                     for chunk in response:
                         chunk_buffer += chunk
-                        if len(chunk_buffer) > 50:
+                        if len(chunk_buffer) > 100:
                             logger.info(f"chunk_buffer: {chunk_buffer}")
-                            yield f"data: {chunk_buffer}\n\n".encode('utf-8')
-                            time.sleep(0.5)
+                            yield f"data: {chunk_buffer}".encode('utf-8')
+                            # time.sleep(0.5)
                             chunk_buffer = ""
-                    yield f"data: {chunk_buffer}\n\n".encode('utf-8')
+                    yield f"data: {chunk_buffer}".encode('utf-8')
             return Response(stream_with_context(stream_response()), content_type='text/event-stream')
         else:
             logger.info(f"response: {response}")
