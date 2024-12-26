0166243695932bd53d838dccd9d5ab470c802a84
Jungmin Lee <starjungmin@gmail.com>
2024-12-26 09:15:30 +0900

    merry_christmas

--- chirstmas.py
+++ chirstmas.py
@@ -1,14 +1,15 @@
+
 def is_christmas():
-    result = ""
-    print('<br>'.join
-          ([''.join
-            ([('Merry Christmas '[(x-y) % 8]
-               if ((x*0.05)**2+(y*0.1)**2-1)
-               ** 3 - (x*0.05)**2*(y*0.1)
-               ** 3 <= 0 else ' ')
-                for x in range(-30, 30)])
-              for y in range(15, -15, -1)]))
-    result += "Merry Christmas"
+    result = "```"
+    result += ('\n'.join
+               ([''.join
+                 ([('Merry Christmas '[(x-y) % 8]
+                    if ((x*0.05)**2+(y*0.1)**2-1)
+                     ** 3 - (x*0.05)**2*(y*0.1)
+                     ** 3 <= 0 else ' ')
+                  for x in range(-30, 30)])
+                for y in range(15, -15, -1)]))
+    result += "Merry Christmas```"
     return result
 
 
